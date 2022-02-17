05b24921998087011e213c4a6138d0f95459fcf8
root <root@msmongomaster.c.rvc-ms.internal>
2022-02-17 18:54:23 +0000

    0.1.7

--- ch02_spark_streaming/spark-kafka-avro-sql.py
+++ ch02_spark_streaming/spark-kafka-avro-sql.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Not working because of a driver issue
 
-# spark-submit --jars /usr/share/java/mysql-connector-java.jar --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1,org.apache.spark:spark-avro_2.12:3.2.1 spark-kafka-avro-consumer.py
+# spark-submit --jars /usr/share/java/mysql-connector-java.jar --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1,org.apache.spark:spark-avro_2.12:3.2.1 spark-kafka-avro-sql.py
 # pyspark --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1,org.apache.spark:spark-avro_2.12:3.2.1
 
 import os, sys, json, io
