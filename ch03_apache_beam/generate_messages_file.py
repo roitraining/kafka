7060a29253578de463ad0ef52f3a06ceacb0d856
root <root@msmongomaster.c.rvc-ms.internal>
2022-02-19 19:12:01 +0000

    0.2.1

--- ch03_apache_beam/generate_messages_file.py
+++ ch03_apache_beam/generate_messages_file.py
@@ -2,7 +2,7 @@
 
 import json
 import random
-import time
+import datetime, time
 import os
 import argparse
 
@@ -12,20 +12,24 @@ import uuid
 stocks = ['AAPL', 'GOOG', 'MSFT']
 base_time = 1645290182886
 with open('trades.txt', 'w') as f:
+    t = datetime.datetime.now()
+    five_minutes = datetime.timedelta(minutes = 5)
     for x in range(1000):
+        t2 = t + five_minutes * x
         for s in range(3):
-            msg = json.dumps({
-                'event_time': str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())),
+            msg = {
+#                'event_time': str(time.strftime("%Y-%m-%d %H:%M:%S", t2)),
+                'event_time': str(t2.strftime("%Y-%m-%d %H:%M:%S")),
                 'symbol': stocks[s],
                 'price': random.randint(10000, 30000)/100,
                 'quantity': random.randint(10, 1000)
-            })
+            }
             key = uuid.uuid4()
-            timestamp = base_time + x * 1000
+            timestamp = time.mktime(t2.timetuple())
 
             data = {"key": str(key), "value": msg
             , "timestamp": timestamp, "offset": x
             , "partition": 0, "timestamp_type" : 0, "topic": "stocks-json"
             }
-            f.write(json.dumps(data))
+            f.write(json.dumps(data) + '\n')
 
