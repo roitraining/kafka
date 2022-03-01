78533ba9e58be1c9eaaf95ac794e399db3a4dd0e
root <root@mskafka1amr.c.rvc-ms.internal>
2022-03-01 13:27:22 +0000

    0.3.0

--- ch01_producers_and_consumers/2-python-kafka-json-consumer.py
+++ ch01_producers_and_consumers/2-python-kafka-json-consumer.py
@@ -1,7 +1,13 @@
 #! /usr/bin/python3
 
+import json
 from kafka import KafkaConsumer
 consumer = KafkaConsumer('stocks-json')
 print("consumer = ", consumer)
 for event in consumer:
-   print('json consumer -', event)
+   #print('json consumer -', event)
+   key = event.key
+   value = event.value
+   value2 = json.loads(value)
+   #print(type(value), type(value2), value2)
+   print(value2['symbol'], value2['quantity'])
