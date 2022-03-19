513905b3e8426e634ba2503c7de931dc61cf6b01
Joseph Gagliardo <joey@me.com>
2022-03-19 19:23:54 +0000

    0.5.0

--- 1-java-producer/src/main/java/com/roi/producer/ProducerMain.java
+++ 1-java-producer/src/main/java/com/roi/producer/ProducerMain.java
@@ -8,15 +8,27 @@ import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer; 
 import org.apache.kafka.clients.producer.Producer; 
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;  
 
 public class ProducerMain {  
     public static void main(String[] args) { 
         Properties props = new Properties(); 
-        props.put("bootstrap.servers", "localhost:9092"); 
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer"); 
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");  
-        props.put("acks", "all");  
+        // props.put("bootstrap.servers", "localhost:9092"); 
+        // props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer"); 
+        // props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");  
+        // props.put("acks", "all");  
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");  
+
+    // props.put(ConsumerConfig.GROUP_ID_CONFIG, CLIENT_ID);
+    // props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+    // props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+    // props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+
         Producer<String, String> producer = new KafkaProducer<>(props);  
         try { 
             File file = new File(ProducerMain.class.getClassLoader().getResource("transactions.txt").getFile()); 
